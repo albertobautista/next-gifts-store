53ef49b736a7c91515950e4a7bfc60d7de3b66ea
Alberto Bautista <dachb101@gmail.com>
2023-07-01 23:13:22 -0600

    Disable api in prod temp

--- src/pages/api/seed.ts
+++ src/pages/api/seed.ts
@@ -10,8 +10,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if (process.env.NODE_ENV === "production")
-    return res.status(401).json({ message: "No tiene acceso al API" });
+  // if (process.env.NODE_ENV === "production")
+  //   return res.status(401).json({ message: "No tiene acceso al API" });
   await db.connect();
 
   await User.deleteMany();
